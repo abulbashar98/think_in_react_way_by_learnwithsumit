9d8ce98b585f14e22be8e695075ac2be70b7dee9
abulbashar98 <abulbashar98116@gmail.com>
2022-11-30 22:28:43 +0600

    useState Hook UseCase Problem 1:- setValue function returned by useState Hook is fundamentally different from setState of Class component, Does not submerge a single chang to the whole Object, keeping the object or Array intact.... Solution of that is we have to copy the whole object or Array Using spread Operator, Then override the value setValue has given

--- src/index.js
+++ src/index.js
@@ -1,72 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// import reportWebVitals from './reportWebVitals';
-
-let states = [] // [0: [value,setterFunc], 1: [value,setterFunc]]
-let stateIndex = -1;
-
-function useState(defaultValue) {
-  const index = ++stateIndex;
-
-  if (states[index]) return states[index]
-
-  const setValue = (newValue) => {
-
-    states[index][0] = newValue;
-    renderWithAbulBashar();
-
-  }
-
-  const returnArray = [defaultValue, setValue];
-  states[index] = returnArray;
-  return returnArray;
-
-}
-
-
-
-const App = () => {
-
-  const [todo, setTodo] = useState('');
-  const [warning, setWarning] = useState(null);
-
-
-  const style = { width: '50%', marginLeft: '500px' }
-
-  const handleChange = (e) => {
-
-    const inputValue = e.target.value;
-
-    const warning = inputValue.includes(".js") ? "You need Advanced JavaScript skill to Handle this, Do you have it??" : null
-
-    setTodo(inputValue);
-    setWarning(warning);
-  }
-
-
-
-  return (
-    <div style={style}>
-      <h2>{todo}</h2>
-      <textarea onChange={handleChange} name="input" id="" ></textarea>
-      <h1>{warning || "Good Choice!"}</h1>
-
-    </div>
-  );
-};
-
-
-function renderWithAbulBashar() {
-  stateIndex = -1;
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
-}
-renderWithAbulBashar();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
