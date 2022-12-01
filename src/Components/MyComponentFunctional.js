3eed26241a40549329e464515afa6adc78b01466
abulbashar98 <abulbashar98116@gmail.com>
2022-12-01 17:49:20 +0600

    A blank Array '[]' in Dependency Works like componentDidMount life cylce, It renders the first time, then because of an empty dependency it witll no t render Again..... A content Array like the previous commit With a dependency works like componenetDidUpdate life Cycle

--- src/Components/MyComponentFunctional.js
+++ src/Components/MyComponentFunctional.js
@@ -5,8 +5,7 @@ import React, { useEffect, useState } from 'react';
 const MyComponentFunctional = () => {
 
     const [count, setCount] = useState(0);
-    const [text, setText] = useState('');
-    // const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
 
     useEffect(() => {
@@ -14,14 +13,14 @@ const MyComponentFunctional = () => {
         document.title = `Clicked ${count} times`
     }, [count])
 
-    // useEffect(() => {
-    //     console.log("Timer Rendered");
-    //     setInterval(tick(), 1000)
-    // }, [])
+    useEffect(() => {
+        console.log("Timer Rendered");
+        setInterval(tick, 1000)
+    }, [])
 
-    // const tick = () => {
-    //     setDate(new Date())
-    // }
+    const tick = () => {
+        setDate(new Date())
+    }
 
     const addClick = () => {
         setCount((prevCount) => prevCount + 1)
@@ -29,10 +28,7 @@ const MyComponentFunctional = () => {
 
     return (
         <div>
-            <p>
-                <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-            </p>
-            {/* <p>Time: {date.toLocaleTimeString()}</p> */}
+            <p>Time: {date.toLocaleTimeString()}</p>
             <p>
                 <button onClick={addClick}>Add Click</button>
             </p>
