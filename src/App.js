905919b88fe59b0f373af8cb724d66e579d7bfb3
abulbashar98 <abulbashar98116@gmail.com>
2022-12-05 18:46:59 +0600

    Use of useRef Hook to get a Dom node into a container, In react's own way of manipulating DOM.... Notice that ref can be used upon any JSX element or Component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,26 @@
-import React, { useState } from "react";
-import { useMemo } from "react";
-import { useCallback } from "react";
-import Button from "./Components/Button";
-import ShowCount from "./Components/ShowCount";
-import Title from "./Components/Title";
+import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 
 
 
 function App() {
 
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const inputRef = useRef();
 
+  useEffect(() => {
 
-  const isEvenOrOdd = useMemo(() => {
-    for (let i = 0; i < 1000000000; i++) {
+    // console.log(inputRef.current)
 
-    }
-    return count1 % 2 === 0;
+    inputRef.current.focus();
 
-  }, [count1])
-
-
-  //  This is when Call Back uses a Dependency,depends on a global variable 
-  // const incrementByOne = useCallback(() => {
-  //   setCount1(count1 + 1)
-  // }, [count1])
-  const incrementByOne = useCallback(() => {
-    setCount1((prevCount) => prevCount + 1)
   }, [])
 
-
-  //  This is when Call Back uses a Dependency,depends on a global variable
-  // const incrementByFive = useCallback(() => {
-  //   setCount2(count2 + 5)
-  // }, [count2])
-  const incrementByFive = useCallback(() => {
-    setCount2((prevCount) => prevCount + 5)
-  }, [])
 
   return (
 
-    <>
-      <Title title="(React.memo) (useCallback) and (useMemo) Tutorial" />
-      <ShowCount title="Counter 1" count={count1} />
-      <p>{isEvenOrOdd ? "Even" : "Odd"}</p>
-      <Button incrementCount={incrementByOne}>Increment By One</Button>
-      <hr />
-      <Title></Title>
-      <ShowCount title="Counter 2" count={count2} />
-      <Button incrementCount={incrementByFive}>Increment By Five</Button>
-    </>
-
+    <input ref={inputRef} type="text" placeholder="Enter Your Name Here" />
 
   );
 
