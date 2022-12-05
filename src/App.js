ff2cb3e871b168568c7bad1bdd7fc5e3558b0d76
abulbashar98 <abulbashar98116@gmail.com>
2022-12-05 08:42:47 +0600

    Solution of Reference memorising Problem Using useCallback Hook with or without dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useCallback } from "react";
 import Button from "./Components/Button";
 import ShowCount from "./Components/ShowCount";
 import Title from "./Components/Title";
@@ -11,15 +12,23 @@ function App() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const incrementByOne = () => {
+
+  //  This is when Call Back uses a Dependency,depends on a global variable 
+  // const incrementByOne = useCallback(() => {
+  //   setCount1(count1 + 1)
+  // }, [count1])
+  const incrementByOne = useCallback(() => {
     setCount1((prevCount) => prevCount + 1)
-  }
+  }, [])
 
-  const incrementByFive = () => {
 
+  //  This is when Call Back uses a Dependency,depends on a global variable
+  // const incrementByFive = useCallback(() => {
+  //   setCount2(count2 + 5)
+  // }, [count2])
+  const incrementByFive = useCallback(() => {
     setCount2((prevCount) => prevCount + 5)
-
-  }
+  }, [])
 
   return (
 
